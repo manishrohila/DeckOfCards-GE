b2d302ca001d5b32534e081965794592169c84db
manishrohila <manishrohila0507@gmail.com>
2024-12-18 22:30:34 +0530

    Add DeckOfCards program to initialize, shuffle, and distribute

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Initialize the deck
+        String[] deck = new String[52];
+        for (int i = 0; i < suits.length; i++) {
+            for (int j = 0; j < ranks.length; j++) {
+                deck[i * ranks.length + j] = ranks[j] + " of " + suits[i];
+            }
+        }
+
+        // Shuffle the deck
+        shuffleDeck(deck);
+
+        // Distribute 9 cards to 4 players
+        String[][] players = new String[4][9];
+        int cardIndex = 0;
+        for (int i = 0; i < players.length; i++) {
+            for (int j = 0; j < players[i].length; j++) {
+                players[i][j] = deck[cardIndex++];
+            }
+        }
+
+        // Print the cards received by each player
+        for (int i = 0; i < players.length; i++) {
+            System.out.println("Player " + (i + 1) + "'s cards:");
+            for (int j = 0; j < players[i].length; j++) {
+                System.out.println(players[i][j]);
+            }
+            System.out.println();
         }
     }
+    public static void shuffleDeck(String[] deck) {
+        Random random = new Random();
+        for (int i = 0; i < deck.length; i++) {
+            int randomIndex = random.nextInt(deck.length);
+            // Swap the cards
+            String temp = deck[i];
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = temp;
+        }
+    }
+
 }
